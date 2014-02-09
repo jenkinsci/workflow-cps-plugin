0ba2db5f7cfea4390539a169682f7ba18f76a583
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-02-09 06:07:17 -0800

    add regular shell on the side

--- src/test/groovy/com/cloudbees/groovy/cps/CpsTransformerTest.groovy
+++ src/test/groovy/com/cloudbees/groovy/cps/CpsTransformerTest.groovy
@@ -11,22 +11,40 @@ import org.junit.Test
  * @author Kohsuke Kawaguchi
  */
 class CpsTransformerTest {
+    /**
+     * CPS-transforming shelll
+     */
+    GroovyShell csh;
+
+    /**
+     * Default groovy shell
+     */
     GroovyShell sh;
+
     def binding = new Binding()
 
     @Before
     void setUp() {
+        def imports = new ImportCustomizer().addImports(CpsTransformerTest.class.name)
+
         def cc = new CompilerConfiguration()
-        cc.addCompilationCustomizers(new ImportCustomizer().addImports(CpsTransformerTest.class.name))
+        cc.addCompilationCustomizers(imports)
         cc.addCompilationCustomizers(new CpsTransformer())
+        csh = new GroovyShell(binding,cc);
+
+        cc = new CompilerConfiguration()
+        cc.addCompilationCustomizers(imports)
         sh = new GroovyShell(binding,cc);
     }
 
     @Test
     void helloWorld() {
-        Function f = sh.evaluate("'hello world'.length()")
-        def p = f.invoke(null, null, [], Continuation.HALT)
+        Script s = csh.parse("'hello world'.length()")
+        Function f = s.run();
+        def p = f.invoke(null, s, [], Continuation.HALT)
 
         assert p.resume().yieldedValue()==11;
+
+        assert sh.evaluate("'hello world'.length()")==11;
     }
 }
