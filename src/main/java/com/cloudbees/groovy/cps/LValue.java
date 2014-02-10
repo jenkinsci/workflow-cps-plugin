3b7f55f9d56d03e146d8bdba8f95925c633924bf
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-02-10 10:26:51 -0800

    doc improvement

--- src/main/java/com/cloudbees/groovy/cps/LValue.java
+++ src/main/java/com/cloudbees/groovy/cps/LValue.java
@@ -10,11 +10,21 @@ package com.cloudbees.groovy.cps;
 public interface LValue {
     /**
      * Computes the value, and passes it to the given continuation when done.
+     *
+     * <p>
+     * Just like {@link Block#eval(Env, Continuation)}, the actual evaluation is done
+     * by the caller by repeatedly {@linkplain Next#step() step executing}
+     * the resulting {@link Next} object.
      */
     Next get(Continuation k);
 
     /**
      * Sets the given value to this variable, and passes {@code null} to the given continuation when done.
+     *
+     * <p>
+     * Just like {@link Block#eval(Env, Continuation)}, the actual evaluation is done
+     * by the caller by repeatedly {@linkplain Next#step() step executing}
+     * the resulting {@link Next} object.
      */
     Next set(Object v, Continuation k);
 }
