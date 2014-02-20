cb80771d16dc663451d316be2b3e538855a35cc3
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-02-20 06:16:56 -0800

    a passing test case for suspend/resume

--- src/test/groovy/com/cloudbees/groovy/cps/ContinuableTest.groovy
+++ src/test/groovy/com/cloudbees/groovy/cps/ContinuableTest.groovy
@@ -1,6 +1,5 @@
 package com.cloudbees.groovy.cps
 
-import com.cloudbees.groovy.cps.impl.CpsFunction
 import org.junit.Test
 
 /**
@@ -11,18 +10,21 @@ import org.junit.Test
 class ContinuableTest extends AbstractGroovyCpsTest {
     @Test
     void resumeAndSuspend() {
-        Script s = csh.parse("""
+        def inv = parseCps("""
             int x = 1;
-            x = Continuable.suspend(x)
-            return x;
+            x = Continuable.suspend(x+1)
+            return x+1;
         """)
-        CpsFunction f = s.run();
-        new Continuable(f.invoke(null,s,[],Continuation.HALT))
 
-        def baos = new ByteArrayOutputStream()
-        new ObjectOutputStream(baos).writeObject();
+        def c = new Continuable(inv.invoke(null,Continuation.HALT));
+        assert c.isResumable()
 
-        Continuable cx = new ObjectInputStreamWithLoader(new ByteArrayInputStream(baos.toByteArray()),s.class.classLoader).readObject()
-        assert 10==cx.run(null)
+        def v = c.run(null);
+        assert v==2 : "Continuable.suspend(x+1) returns the control back to us";
+
+        assert c.isResumable() : "Continuable is resumable because it has 'return x+1' to execute."
+        assert c.run(3)==4 : "We resume continuable, then the control comes back from the return statement"
+
+        assert !c.isResumable() : "We've run the program till the end, so it's no longer resumable"
     }
 }
