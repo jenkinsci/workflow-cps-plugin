6ccdb2bc58d6e76b5a0bef1d1e3c46739e7f11d8
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-05-02 08:37:07 -0700

    Fix up stack trace for exceptions thrown from synchronous code invoked from CPS code

--- src/main/java/com/cloudbees/groovy/cps/impl/ContinuationGroup.java
+++ src/main/java/com/cloudbees/groovy/cps/impl/ContinuationGroup.java
@@ -1,6 +1,7 @@
 package com.cloudbees.groovy.cps.impl;
 
 import com.cloudbees.groovy.cps.Block;
+import com.cloudbees.groovy.cps.Continuable;
 import com.cloudbees.groovy.cps.Continuation;
 import com.cloudbees.groovy.cps.Env;
 import com.cloudbees.groovy.cps.Next;
@@ -9,6 +10,11 @@ import org.codehaus.groovy.runtime.callsite.CallSite;
 import org.codehaus.groovy.runtime.callsite.CallSiteArray;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.cloudbees.groovy.cps.impl.SourceLocation.UNKNOWN;
 
 /**
  * Base class for defining a series of {@link Continuation} methods that share the same set of contextual values.
@@ -63,8 +69,83 @@ abstract class ContinuationGroup implements Serializable {
         } catch (CpsCallableInvocation inv) {
             return inv.invoke(e, loc, k);
         } catch (Throwable t) {
+            fixupStackTrace(t,loc,e,new ReferenceStackTrace());
             return throwException(e, t);
         }
+    }
+
+    /**
+     * Fix up the stack trace of an exception thrown from synchronous code.
+     *
+     * @param t
+     *      Exception thrown
+     * @param loc
+     *      Location of the call site in the script. null if unknown.
+     * @param e
+     *      Environment that represents the call stack of the asynchronous code
+     * @param ref
+     *      Reference stack trace that identifies the call site. Create this exception in the same
+     *      function that you call into {@link CallSite}. Used to identify the section of {@coe t.getStackTrace()}
+     *      that belong to the caller of groovy-cps and the invocation of {@link CallSite}  induced by the Groovy script.
+     */
+    protected void fixupStackTrace(Throwable t, SourceLocation loc, Env e, ReferenceStackTrace ref) {
+        StackTraceElement[] rs = ref.getStackTrace();
+        StackTraceElement[] ts = t.getStackTrace();
+
+        if (!hasSameRoots(rs,ts)) {
+            // this exception doesn't match up with what we expected.
+            // maybe it was created elsewhere and thrown here?
+            return;
+        }
+
+        /*
+            SYNC TRACE
+            CPS TRACE
+            REFERENECE TRACE
+         */
+
+        List<StackTraceElement> orig = Arrays.asList(ts);
+        int pos = ts.length-rs.length;
+        List<StackTraceElement> stack = new ArrayList<StackTraceElement>(orig.subList(0,pos));
+
+        stack.add((loc!=null ? loc : UNKNOWN).toStackTrace());
+        e.buildStackTraceElements(stack,Integer.MAX_VALUE);
+        stack.add(Continuable.SEPARATOR_STACK_ELEMENT);
+
+        stack.addAll(orig.subList(pos, orig.size()));
+
+        t.setStackTrace(stack.toArray(new StackTraceElement[stack.size()]));
+    }
+
+    /**
+     * Returns true if 'rs' is at the bottom of 'ts'.
+     */
+    private boolean hasSameRoots(StackTraceElement[] rs, StackTraceElement[] ts) {
+        int b = ts.length-rs.length;
+        if (b<0)    return false;
+
+        {// the top of the stack will have different line number because ReferenceStackTrace is created in a separate line
+            StackTraceElement lhs = ts[b];
+            StackTraceElement rhs = rs[0];
+
+            if (!eq(lhs.getClassName(),rhs.getClassName())
+             || !eq(lhs.getMethodName(),rhs.getMethodName())
+             || !eq(lhs.getFileName(),rhs.getFileName()))
+                return false;
+        }
+
+        for (int i=1; i<rs.length; i++) {
+            if (!ts[b+i].equals(rs[i]))
+                return false;
+        }
+
+        return true;
+    }
+
+    private boolean eq(Object x, Object y) {
+        if (x==y)   return true;
+        if (x==null || y==null) return false;
+        return x.equals(y);
     }
 
     /**
---
+++ src/test/groovy/com/cloudbees/groovy/cps/impl/FunctionCallBlockTest.groovy
@@ -0,0 +1,57 @@
+package com.cloudbees.groovy.cps.impl
+
+import com.cloudbees.groovy.cps.AbstractGroovyCpsTest
+import org.junit.Test
+
+import javax.naming.NamingException
+
+/**
+ *
+ *
+ * @author Kohsuke Kawaguchi
+ */
+class FunctionCallBlockTest extends AbstractGroovyCpsTest {
+    /**
+     * Synchronous code we call from test that throws an exception
+     */
+    public static void someSyncCode(int i) {
+        if (i>0)
+            someSyncCode(i-1);
+        else
+            throw new NamingException();
+    }
+
+    @Test
+    void stackTraceFixup() {
+        List elements = evalCPSonly("""
+            @WorkflowMethod
+            def x() {
+              y();  // line 4
+            }
+            @WorkflowMethod
+            def y() {
+              FunctionCallBlockTest.someSyncCode(3); // line 8
+            }
+            try {
+              x(); // line 11
+            } catch (Exception e) {
+              return e.stackTrace;
+            }
+        """)
+
+        println elements;
+
+        def trace = elements.join("\n")
+
+        // should include the transformed CPS part
+        assert trace.contains("""
+Script1.y(Script1.groovy:8)
+Script1.x(Script1.groovy:4)
+Script1.run(Script1.groovy:11)
+___cps.transform___(Native Method)
+com.cloudbees.groovy.cps.impl.ContinuationGroup.methodCall""");
+
+        // should include the call stack of some sync code
+        assert trace.contains("com.cloudbees.groovy.cps.impl.FunctionCallBlockTest.someSyncCode(FunctionCallBlockTest.groovy:")
+    }
+}
