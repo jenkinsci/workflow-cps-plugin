0451a2f5fc3e742cbdd3b3055a99305dd29e5a78
Jesse Glick <jglick@cloudbees.com>
2016-05-11 16:52:13 -0400

    [JENKINS-27421] Reproduced LinkedHashMap.Entry error in test, and capturing workaround.

--- src/test/java/org/jenkinsci/plugins/workflow/SerializationTest.java
+++ src/test/java/org/jenkinsci/plugins/workflow/SerializationTest.java
@@ -184,6 +184,47 @@ public class SerializationTest extends SingleJobTestBase {
         });
     }
 
+    @Ignore("TODO java.io.NotSerializableException: java.util.LinkedHashMap$Entry")
+    @Issue("JENKINS-27421")
+    @Test public void mapIterator() {
+        story.addStep(new Statement() {
+            @Override public void evaluate() throws Throwable {
+                p = jenkins().createProject(WorkflowJob.class, "demo");
+                ScriptApproval.get().approveSignature("staticMethod org.codehaus.groovy.runtime.DefaultGroovyMethods collect java.util.Map groovy.lang.Closure");
+                ScriptApproval.get().approveSignature("method java.util.Map entrySet");
+                p.setDefinition(new CpsFlowDefinition(
+                    "def map = [one: 1, two: 2]\n" +
+                    "@NonCPS def entries(m) {m.collect {k, v -> [k, v]}}; mapE = entries(map); for (int i = 0; i < mapE.size(); i++) {def e = mapE[i]; echo \"running C-style loop on ${e[0]} → ${e[1]}\"; semaphore \"C-${e[0]}\"}\n" +
+                    "for (def e : map.entrySet()) {echo \"running new-style loop on ${e.key} → ${e.value}\"; semaphore \"new-${e.key}\"}"
+                    // TODO check also keySet(), values()
+                    , true));
+                startBuilding();
+                SemaphoreStep.waitForStart("C-one/1", b);
+                story.j.waitForMessage("running C-style loop on one → 1", b);
+            }
+        });
+        story.addStep(new Statement() {
+            @Override public void evaluate() throws Throwable {
+                rebuildContext(story.j);
+                SemaphoreStep.success("C-one/1", null);
+                SemaphoreStep.success("C-two/1", null);
+                story.j.waitForMessage("running C-style loop on two → 2", b);
+                SemaphoreStep.waitForStart("new-one/1", b);
+                story.j.waitForMessage("running new-style loop on one → 1", b);
+            }
+        });
+        story.addStep(new Statement() {
+            @Override public void evaluate() throws Throwable {
+                rebuildContext(story.j);
+                SemaphoreStep.success("new-one/1", null);
+                SemaphoreStep.success("new-two/1", null);
+                story.j.waitForCompletion(b);
+                story.j.assertBuildStatusSuccess(b);
+                story.j.assertLogContains("running new-style loop on two → 2", b);
+            }
+        });
+    }
+
     @Test public void nonCps() {
         story.addStep(new Statement() {
             @Override public void evaluate() throws Throwable {
