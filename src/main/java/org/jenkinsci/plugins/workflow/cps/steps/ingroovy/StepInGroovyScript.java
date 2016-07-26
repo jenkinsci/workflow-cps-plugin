fabf2538a7ca61ab81d97802ac52dab9d71515a3
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-07-26 13:43:19 -0700

    Needs to extend from CpsScript
    ... so that we can call other steps

--- src/main/java/org/jenkinsci/plugins/workflow/cps/steps/ingroovy/StepInGroovyScript.java
+++ src/main/java/org/jenkinsci/plugins/workflow/cps/steps/ingroovy/StepInGroovyScript.java
@@ -1,12 +1,22 @@
 package org.jenkinsci.plugins.workflow.cps.steps.ingroovy;
 
-import groovy.lang.Script;
+import org.jenkinsci.plugins.workflow.cps.CpsScript;
+import org.jenkinsci.plugins.workflow.cps.persistence.PersistIn;
+
+import java.io.IOException;
+
+import static org.jenkinsci.plugins.workflow.cps.persistence.PersistenceContext.*;
 
 /**
- * Base class that defines conract for steps written in Groovy.
+ * Base class that defines contract for steps written in Groovy.
+ *
  * @author Kohsuke Kawaguchi
  * @see GroovyCompiler
  */
-public abstract class StepInGroovyScript extends Script {
+@PersistIn(PROGRAM)
+public abstract class StepInGroovyScript extends CpsScript {
+    public StepInGroovyScript() throws IOException {
+    }
+
     public abstract String getDisplayName();
 }
