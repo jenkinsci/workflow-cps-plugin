6f029914adca861070728b4ca917a3cb035bd307
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-09-07 08:04:54 -0700

    Not meant for external consumption

--- src/main/java/org/jenkinsci/plugins/workflow/cps/Safepoint.java
+++ src/main/java/org/jenkinsci/plugins/workflow/cps/Safepoint.java
@@ -3,13 +3,16 @@ package org.jenkinsci.plugins.workflow.cps;
 import com.cloudbees.groovy.cps.Continuable;
 import com.cloudbees.groovy.cps.Outcome;
 import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.Whitelisted;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
 
 /**
  * Safepoint called from the CPS interpreter.
  *
  * @author Kohsuke Kawaguchi
  */
-// classes compiled from Pipeline Script refers to this class, so it needs to be public, but
+// classes compiled from Pipeline Script refers to this class, so it needs to be public, but not meant for external consumption
+@Restricted(NoExternalUse.class)
 public class Safepoint extends ThreadTask {
     /**
      * Method called from CPS interpreter.
