0e2a049ce635a71a3b341ecf959350c628675ae9
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-09-19 19:46:19 -0700

    Test case that reproduces the problem

---
+++ src/test/java/org/jenkinsci/plugins/workflow/cps/CpsBodyExecutionTest.java
@@ -0,0 +1,86 @@
+package org.jenkinsci.plugins.workflow.cps;
+
+import groovy.lang.Closure;
+import hudson.model.Result;
+import org.codehaus.groovy.runtime.ScriptBytecodeAdapter;
+import org.jenkinsci.plugins.workflow.cps.CpsBodyExecutionTest.SynchronousExceptionInBodyStep.EmperorHasNoClothes;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.jenkinsci.plugins.workflow.steps.AbstractStepDescriptorImpl;
+import org.jenkinsci.plugins.workflow.steps.AbstractStepExecutionImpl;
+import org.jenkinsci.plugins.workflow.steps.AbstractStepImpl;
+import org.jenkinsci.plugins.workflow.steps.BodyExecutionCallback;
+import org.junit.Test;
+import org.jvnet.hudson.test.TestExtension;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import javax.annotation.Nonnull;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class CpsBodyExecutionTest extends AbstractCpsFlowTest {
+    /**
+     * When the body of a step is synchronous and explodes, the failure should be recorded and the pipeline job
+     * should move on.
+     *
+     * But instead, this hangs because CpsBodyExecution has a bug in how it handles this case.
+     * It tries to launch the body (in this case the 'bodyBlock' method) in a separate CPS thread,
+     * and puts the parent CPS thread on hold. Yet when the child CPS thread ends with an exception,
+     * it fails to record this result correctly, and it gets into the eternal sleep in which
+     * the parent CPS thread expects to be notified of the outcome of the child CPS thread, which
+     * never arrives.
+     */
+    @Test
+    public void synchronousExceptionInBody() throws Exception {
+        WorkflowJob p = jenkins.createProject(WorkflowJob.class);
+        p.setDefinition(new CpsFlowDefinition("synchronousExceptionInBody()",true));
+
+        WorkflowRun b = jenkins.assertBuildStatus(Result.FAILURE, p.scheduleBuild2(0));
+        jenkins.assertLogContains(EmperorHasNoClothes.class.getName(),b);
+    }
+
+    public static class SynchronousExceptionInBodyStep extends AbstractStepImpl {
+        @DataBoundConstructor
+        public SynchronousExceptionInBodyStep() {}
+
+        @TestExtension
+        public static class DescriptorImpl extends AbstractStepDescriptorImpl {
+            public DescriptorImpl() {
+                super(Execution.class);
+            }
+
+            @Override
+            public String getFunctionName() {
+                return "synchronousExceptionInBody";
+            }
+        }
+
+        public static class Execution extends AbstractStepExecutionImpl {
+            /**
+             * Invoked as a body that induces a synchronous exception
+             */
+            public void bodyBlock() {
+                throw new EmperorHasNoClothes();
+            }
+
+            @Override
+            public boolean start() throws Exception {
+                Closure body = ScriptBytecodeAdapter.getMethodPointer(this, "bodyBlock");
+                CpsStepContext cps = (CpsStepContext) getContext();
+                CpsThread t = CpsThread.current();
+                cps.newBodyInvoker(t.getGroup().export(body))
+                        .withCallback(BodyExecutionCallback.wrap(cps))
+                        .start();
+                return false;
+            }
+
+            @Override
+            public void stop(@Nonnull Throwable cause) throws Exception {
+                throw new UnsupportedOperationException();
+            }
+        }
+
+        public static class EmperorHasNoClothes extends RuntimeException {}
+    }
+}
