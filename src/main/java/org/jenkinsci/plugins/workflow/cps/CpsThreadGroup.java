fab9a6275f2e83b182405aeea2147bf6611c358b
Jesse Glick <jglick@cloudbees.com>
2016-11-08 17:08:02 -0500

    scripts might be null if loading old builds.

--- src/main/java/org/jenkinsci/plugins/workflow/cps/CpsThreadGroup.java
+++ src/main/java/org/jenkinsci/plugins/workflow/cps/CpsThreadGroup.java
@@ -62,6 +62,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import static java.util.logging.Level.*;
+import javax.annotation.CheckForNull;
 import static org.jenkinsci.plugins.workflow.cps.CpsFlowExecution.*;
 import static org.jenkinsci.plugins.workflow.cps.persistence.PersistenceContext.*;
 
@@ -122,7 +123,7 @@ public final class CpsThreadGroup implements Serializable {
      */
     public final Map<Integer,Closure> closures = new HashMap<Integer,Closure>();
 
-    private final List<Script> scripts = new ArrayList<>();
+    private final @CheckForNull List<Script> scripts = new ArrayList<>();
 
     CpsThreadGroup(CpsFlowExecution execution) {
         this.execution = execution;
@@ -361,7 +362,9 @@ public final class CpsThreadGroup implements Serializable {
         }
         if (ending) {
             execution.cleanUpHeap();
-            scripts.clear();
+            if (scripts != null) {
+                scripts.clear();
+            }
             closures.clear();
         }
 
