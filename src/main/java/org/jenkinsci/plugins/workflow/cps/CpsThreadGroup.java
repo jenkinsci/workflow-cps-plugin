c0505e775177e82221c0e9dc9a34b1d196efb62b
Jesse Glick <jglick@cloudbees.com>
2017-01-10 15:02:57 -0500

    Removing simplistic superseded logging.

--- src/main/java/org/jenkinsci/plugins/workflow/cps/CpsThreadGroup.java
+++ src/main/java/org/jenkinsci/plugins/workflow/cps/CpsThreadGroup.java
@@ -420,9 +420,6 @@ public final class CpsThreadGroup implements Serializable {
 
     @CpsVmThreadOnly
     public void saveProgram(File f) throws IOException {
-        boolean logging = LOGGER.isLoggable(Level.FINER);
-        long start = logging ? System.nanoTime() : 0;
-
         File dir = f.getParentFile();
         File tmpFile = File.createTempFile("atomic",null, dir);
 
@@ -456,11 +453,6 @@ public final class CpsThreadGroup implements Serializable {
             PROGRAM_STATE_SERIALIZATION.set(old);
             execution.time(TimingKind.save, true);
             Util.deleteFile(tmpFile);
-        }
-
-        if (logging) {
-            long end = System.nanoTime();
-            LOGGER.log(FINER, "saved {0} of size {1}Kb in {2}ms", new Object[] {f, f.length() / 1000, (end - start) / 1000 / 1000});
         }
     }
 
