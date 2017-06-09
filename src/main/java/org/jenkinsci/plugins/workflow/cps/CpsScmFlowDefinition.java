d1cf9a40fac981a4c816dba9564b317f57b65636
Matthew Montgomery <matthew@signed8bit.com>
2017-06-09 07:46:52 -0500

    Adds option to treat missing script file as an empty file
    When automating project creation along with base Jenkins jobs
    for various projects, the current behavior of the Groovy plugin requires
    a development team to create an empty script file before they may be
    ready to develop a proper pipeline in order to pass CI. This becomes
    a barrier to a development team when the failure of the initial CI job
    provides a negative code review on a change.
    
    This commit adds an option to treat a missing script file as an empty
    file thus allowing the CI job to pass. The default for this option is
    "false" thus preserving the original behavior of the plugin.

--- src/main/java/org/jenkinsci/plugins/workflow/cps/CpsScmFlowDefinition.java
+++ src/main/java/org/jenkinsci/plugins/workflow/cps/CpsScmFlowDefinition.java
@@ -61,6 +61,7 @@ public class CpsScmFlowDefinition extends FlowDefinition {
 
     private final SCM scm;
     private final String scriptPath;
+    private boolean ignoreMissingScript;
     private boolean lightweight;
 
     @DataBoundConstructor public CpsScmFlowDefinition(SCM scm, String scriptPath) {
@@ -74,6 +75,14 @@ public class CpsScmFlowDefinition extends FlowDefinition {
 
     public String getScriptPath() {
         return scriptPath;
+    }
+
+    public boolean isIgnoreMissingScript() {
+        return ignoreMissingScript;
+    }
+
+    @DataBoundSetter public void setIgnoreMissingScript(boolean ignoreMissingScript) {
+        this.ignoreMissingScript = ignoreMissingScript;
     }
 
     public boolean isLightweight() {
@@ -98,8 +107,19 @@ public class CpsScmFlowDefinition extends FlowDefinition {
         if (isLightweight()) {
             try (SCMFileSystem fs = SCMFileSystem.of(build.getParent(), scm)) {
                 if (fs != null) {
-                    String script = fs.child(scriptPath).contentAsString();
-                    listener.getLogger().println("Obtained " + scriptPath + " from " + scm.getKey());
+                    String script;
+                    try {
+                        script = fs.child(scriptPath).contentAsString();
+                        listener.getLogger().println("Obtained " + scriptPath + " from " + scm.getKey());
+                    } catch (java.io.FileNotFoundException e) {
+                        if (isIgnoreMissingScript()) {
+                            // Set script to am empty string to mimic an empty scriptFile
+                            script = "";
+                            listener.getLogger().println("The file " + scriptPath + " from " + scm.getKey() + " was not found, ignoring");
+                        } else {
+                            throw e;
+                        }
+                    }
                     return new CpsFlowExecution(script, true, owner);
                 } else {
                     listener.getLogger().println("Lightweight checkout support not available, falling back to full checkout.");
@@ -133,9 +153,17 @@ public class CpsScmFlowDefinition extends FlowDefinition {
                 throw new IOException(scriptFile + " is not inside " + dir);
             }
             if (!scriptFile.exists()) {
-                throw new AbortException(scriptFile + " not found");
+                if (isIgnoreMissingScript()) {
+                    // Set script to am empty string to mimic an empty scriptFile
+                    script = "";
+                    listener.getLogger().println("The file " + scriptPath + " from " + scm.getKey() + " was not found, ignoring");
+                } else {
+                    throw new AbortException(scriptFile + " not found");
+                }
+            } else {
+                script = scriptFile.readToString();
             }
-            script = scriptFile.readToString();
+
         }
         CpsFlowExecution exec = new CpsFlowExecution(script, true, owner);
         exec.flowStartNodeActions.add(new WorkspaceActionImpl(dir, null));
