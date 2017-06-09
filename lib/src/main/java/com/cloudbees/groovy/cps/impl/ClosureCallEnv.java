ad1208eb7175e140e1032228db370b51d196d012
Andrew Bayer <andrew.bayer@gmail.com>
2017-06-09 16:00:24 -0400

    [FIXED JENKINS-38268] Proper lexical scoping for closure variables.
    Previously, any variables that hadn't been explicitly declared (via
    `ClosureCallEnv.declareVariable`) in a `Closure` would end up getting
    set in the captured `Env`. Which means we'd end up setting things
    globally when we really didn't mean to. This changes that to only set
    a variable in the captured `Env` if there's no `locals` variable
    defined by that name *and* the captured `Env` has a non-null return
    for `getVariableType(name)` - i.e., the variable name has already been
    declared in the captured `Env`.

--- lib/src/main/java/com/cloudbees/groovy/cps/impl/ClosureCallEnv.java
+++ lib/src/main/java/com/cloudbees/groovy/cps/impl/ClosureCallEnv.java
@@ -3,6 +3,7 @@ package com.cloudbees.groovy.cps.impl;
 import com.cloudbees.groovy.cps.Continuation;
 import com.cloudbees.groovy.cps.Env;
 
+import javax.annotation.Nonnull;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,23 +28,24 @@ class ClosureCallEnv extends CallEnv {
         this.captured = captured;
     }
 
-    public void declareVariable(Class type, String name) {
+    public void declareVariable(@Nonnull Class type, @Nonnull String name) {
         locals.put(name,null);
         getTypes().put(name, type);
     }
 
-    public Object getLocalVariable(String name) {
+    public Object getLocalVariable(@Nonnull String name) {
         if (locals.containsKey(name))
             return locals.get(name);
         else
             return captured.getLocalVariable(name);
     }
 
-    public void setLocalVariable(String name, Object value) {
-        if (locals.containsKey(name))
-            locals.put(name, value);
-        else
+    public void setLocalVariable(@Nonnull String name, Object value) {
+        if (!locals.containsKey(name) && captured.getLocalVariableType(name) != null) {
             captured.setLocalVariable(name, value);
+        } else {
+            locals.put(name, value);
+        }
     }
 
     public Class getLocalVariableType(String name) {
