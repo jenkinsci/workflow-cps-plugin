136ab41eb266b8b2e1e76f2674ba9882e50e28e5
Sam Van Oort <samvanoort@gmail.com>
2017-10-18 17:55:44 -0400

    Heh, no need to code against something that cannot happen

--- lib/src/main/java/com/cloudbees/groovy/cps/impl/CpsClosureDef.java
+++ lib/src/main/java/com/cloudbees/groovy/cps/impl/CpsClosureDef.java
@@ -29,21 +29,18 @@ class CpsClosureDef extends CpsCallable {
 
     @Override
     Next invoke(Env caller, SourceLocation loc, Object receiver, List<?> args, Continuation k) {
-        Env e = new ClosureCallEnv(caller, k, loc, capture, self, args.size());
+        ClosureCallEnv e = new ClosureCallEnv(caller, k, loc, capture, self, args.size());
 
         assignArguments(args, e);
 
         // TODO: who handles 'it' ?
 
-        // 'e' will be a DepthTrackingEnv for all pipelines begun with this version of the library
-        // but if we upgrade groovy-cps and resume older pipelines, they won't be.
-        if (e instanceof DepthTrackingEnv && ((DepthTrackingEnv)e).getDepth() > DepthTrackingEnv.MAX_LEGAL_DEPTH) {
+        if (e.getDepth() > DepthTrackingEnv.MAX_LEGAL_DEPTH) {
             StackOverflowError err;
-            int depth = ((DepthTrackingEnv) e).getDepth();
             if (loc != null) {
-                err = new StackOverflowError("Excessively nested closures/functions at "+loc+" - look for unbounded recursion - call depth: "+depth);
+                err = new StackOverflowError("Excessively nested closures/functions at "+loc+" - look for unbounded recursion - call depth: "+e.getDepth());
             } else {
-                err = new StackOverflowError("Excessively nested closures/functions - look for unbounded recursion - call depth: "+depth);
+                err = new StackOverflowError("Excessively nested closures/functions - look for unbounded recursion - call depth: "+e.getDepth());
             }
             return e.getExceptionHandler(StackOverflowError.class).receive(err);
         }
