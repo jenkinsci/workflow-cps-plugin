94608bb902f790e66f75ba49451b9684207f05c0
Andrew Bayer <andrew.bayer@gmail.com>
2018-02-26 16:27:05 -0500

    [FIXED JENKINS-42471] Switch to Declarative in samples

--- src/main/js/samples.js
+++ src/main/js/samples.js
@@ -46,38 +46,84 @@ samples.push({
     name: 'hello',
     title: 'Hello World',
     script:
-        "node {\n" +
-        "   echo 'Hello World'\n" +
+        "pipeline {\n" +
+        "   agent any\n" +
+        "\n" +
+        "   stages {\n" +
+        "      stage('Hello') {\n" +
+        "         steps {\n" +
+        "            echo 'Hello World'\n" +
+        "         }\n" +
+        "      }\n" +
+        "   }\n" +
         "}"
-}); 
+});
 
 samples.push({
     name: 'github-maven',
     title: 'GitHub + Maven',
     script:
-        "node {\n" +
-        "   def mvnHome\n" +
-        "   stage('Preparation') { // for display purposes\n" +
-        "      // Get some code from a GitHub repository\n" +
-        "      git 'https://github.com/jglick/simple-maven-project-with-tests.git'\n" +
-        "      // Get the Maven tool.\n" +
-        "      // ** NOTE: This 'M3' Maven tool must be configured\n" +
-        "      // **       in the global configuration.           \n" +
-        "      mvnHome = tool 'M3'\n" +
+        "pipeline {\n" +
+        "   agent any\n" +
+        "\n" +
+        "   tools {\n" +
+        "      // Install the Maven version configured as \"M3\" and add it to the path.\n" +
+        "      maven \"M3\"\n" +
         "   }\n" +
-        "   stage('Build') {\n" +
-        "      // Run the maven build\n" +
-        "      withEnv([\"MVN_HOME=$mvnHome\"]) {\n" +
-        "         if (isUnix()) {\n" +
-        "            sh '\"$MVN_HOME/bin/mvn\" -Dmaven.test.failure.ignore clean package'\n" +
-        "         } else {\n" +
-        "            bat(/\"%MVN_HOME%\\bin\\mvn\" -Dmaven.test.failure.ignore clean package/)\n" +
+        "\n" +
+        "   stages {\n" +
+        "      stage('Build') {\n" +
+        "         steps {\n" +
+        "            // Get some code from a GitHub repository\n" +
+        "            git 'https://github.com/jglick/simple-maven-project-with-tests.git'\n" +
+        "\n" +
+        "            // Run Maven on a Unix agent.\n" +
+        "            sh \"mvn -Dmaven.test.failure.ignore=true clean package\"\n" +
+        "\n" +
+        "            // To run Maven on a Windows agent, use\n" +
+        "            // bat \"mvn -Dmaven.test.failure.ignore=true clean package\"\n" +
+        "         }\n" +
+        "\n" +
+        "         post {\n" +
+        "            // If Maven was able to run the tests, even if some of the test\n" +
+        "            // failed, record the test results and archive the jar file.\n" +
+        "            success {\n" +
+        "               junit '**/target/surefire-reports/TEST-*.xml'\n" +
+        "               archiveArtifacts 'target/*.jar'\n" +
+        "            }\n" +
         "         }\n" +
         "      }\n" +
         "   }\n" +
-        "   stage('Results') {\n" +
-        "      junit '**/target/surefire-reports/TEST-*.xml'\n" + // assumes junit & workflow-basic-steps up to date
-        "      archiveArtifacts 'target/*.jar'\n" +
-        "   }\n" +
         "}"
+});
+
+samples.push({
+    name: 'scripted',
+    title: 'Scripted Pipeline',
+    script:
+    "node {\n" +
+    "   def mvnHome\n" +
+    "   stage('Preparation') { // for display purposes\n" +
+    "      // Get some code from a GitHub repository\n" +
+    "      git 'https://github.com/jglick/simple-maven-project-with-tests.git'\n" +
+    "      // Get the Maven tool.\n" +
+    "      // ** NOTE: This 'M3' Maven tool must be configured\n" +
+    "      // **       in the global configuration.           \n" +
+    "      mvnHome = tool 'M3'\n" +
+    "   }\n" +
+    "   stage('Build') {\n" +
+    "      // Run the maven build\n" +
+    "      withEnv([\"MVN_HOME=$mvnHome\"]) {\n" +
+    "         if (isUnix()) {\n" +
+    "            sh '\"$MVN_HOME/bin/mvn\" -Dmaven.test.failure.ignore clean package'\n" +
+    "         } else {\n" +
+    "            bat(/\"%MVN_HOME%\\bin\\mvn\" -Dmaven.test.failure.ignore clean package/)\n" +
+    "         }\n" +
+    "      }\n" +
+    "   }\n" +
+    "   stage('Results') {\n" +
+    "      junit '**/target/surefire-reports/TEST-*.xml'\n" + // assumes junit & workflow-basic-steps up to date
+    "      archiveArtifacts 'target/*.jar'\n" +
+    "   }\n" +
+    "}"
 });
