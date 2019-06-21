6f358e465260c567eb7c7be491c4f4a814801796
Karl Shultz <kshultz@cloudbees.com>
2019-06-21 14:29:33 -0400

    Pull the cheat back out since I verified correct SS version

--- src/test/java/org/jenkinsci/plugins/workflow/cps/CpsFlowDefinition2Test.java
+++ src/test/java/org/jenkinsci/plugins/workflow/cps/CpsFlowDefinition2Test.java
@@ -86,8 +86,6 @@ public class CpsFlowDefinition2Test extends AbstractCpsFlowTest {
      */
     @Test
     public void endlessRecursion() throws Exception {
-        // Cheat so I can make sure I'm running the version of script-security that I think I am.
-        // Thread.sleep(30000000);
         Assume.assumeTrue(!Functions.isWindows());  // Sidestep false failures specific to a few Windows build environments.
         String script = "def getThing(){return thing == null}; \n" +
                 "node { echo getThing(); } ";
