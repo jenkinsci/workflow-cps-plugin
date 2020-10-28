177928b66f2e4f60d1e97f4f853882fc9ac31734
Félix Queiruga <fqueiruga@cloudbees.com>
2020-10-28 12:35:45 +0100

    Fixes resizing of the editor
    - This fix consists on just adding CSS to make the resizable handler visible
    - Use this opportunity to introduce LESS compilation on the frontend toolchain
    - Makes the styles forwards-compatible with newer Jenkins versions using CSS variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const webpack = require('webpack');
 const path = require('path');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin: CleanPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
-    "workflow-editor": [path.join(__dirname, 'src/main/js/workflow-editor.js')],
+    "workflow-editor": [
+      path.join(__dirname, 'src/main/js/workflow-editor.js'),
+      path.join(__dirname, 'src/main/less/workflow-editor.less'),
+    ],
   },
   output: {
     path: path.join(
@@ -14,10 +18,17 @@ module.exports = {
     ),
   },
   plugins: [
+    new MiniCSSExtractPlugin({
+      filename: "[name].css",
+    }),
     new CleanPlugin(),
   ],
   module: {
     rules: [
+      {
+        test: /\.(css|less)$/,
+        use: [ MiniCSSExtractPlugin.loader, "css-loader", "postcss-loader", "less-loader"]
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
