eb7d2e2a701b4faa954be1031cfe3b2b1828fc49
Basil Crow <me@basilcrow.com>
2022-03-22 13:09:55 -0700

    Threads can never be null now

--- src/main/java/org/jenkinsci/plugins/workflow/cps/CpsThreadGroup.java
+++ src/main/java/org/jenkinsci/plugins/workflow/cps/CpsThreadGroup.java
@@ -183,9 +183,7 @@ public final class CpsThreadGroup implements Serializable {
         execution = CpsFlowExecution.PROGRAM_STATE_SERIALIZATION.get();
         setupTransients();
         assert execution!=null;
-        if (threads != null) {
-            runtimeThreads.putAll(threads);
-        }
+        runtimeThreads.putAll(threads);
         if (/* compatibility: the field will be null in old programs */ scripts != null && !scripts.isEmpty()) {
             GroovyShell shell = execution.getShell();
             // Take the canonical bindings from the main script and relink that object with that of the shell and all other loaded scripts which kept the same bindings.
