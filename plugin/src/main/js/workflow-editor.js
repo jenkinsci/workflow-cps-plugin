bf6cd9263589016ce4bfe1b783b7ee8b2ebd37ba
Basil Crow <me@basilcrow.com>
2023-02-08 11:40:56 -0800

    [JENKINS-70564] Replay page broken after the Ace editor update

--- plugin/src/main/js/workflow-editor.js
+++ plugin/src/main/js/workflow-editor.js
@@ -9,6 +9,7 @@ import { addSamplesWidget } from './samples';
 import ace from "ace-builds/src-noconflict/ace";
 import "ace-builds/src-noconflict/ext-language_tools";
 import "ace-builds/src-noconflict/mode-groovy";
+import "ace-builds/src-noconflict/snippets/javascript";
 import "ace-builds/src-noconflict/theme-tomorrow";
 
 // Import custom snippets
@@ -16,6 +17,7 @@ import "./snippets/workflow";
 
 var editorIdCounter = 0;
 
+document.addEventListener("DOMContentLoaded", function() {
         $('.workflow-editor-wrapper').each(function() {
             initEditor($(this));
         });
@@ -128,3 +130,4 @@ var editorIdCounter = 0;
             wrapper.show();
             textarea.hide();
         }
+});
