fb4d9b79e83a07f9110f3b65c7ede988baa94af1
Basil Crow <me@basilcrow.com>
2023-05-31 10:20:03 -0700

    Drop support for ancient web browsers

--- plugin/.eslintrc.js
+++ plugin/.eslintrc.js
@@ -5,31 +5,16 @@ module.exports = {
     root: true,
     env: {
         browser: true,
-        es6: true
+        es2022: true
     },
     // Uses eslint default ruleset
     extends: "eslint:recommended",
     parserOptions: {
-        ecmaVersion: 2018,
+        ecmaVersion: 2022,
         sourceType: "module"
     },
     rules: {
-    },
-    globals: {
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly",
-
-        '__dirname': false,
-
-        // Allow jest globals used in tests
-        jest: false,
-        expect: false,
-        it: false,
-        describe: false,
-        beforeEach: false,
-        afterEach: false,
-        beforeAll: false,
-        afterAll: false,
+        curly: "error",
     }
   }; 
 
