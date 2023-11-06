caa79344f57561674eebc52f8c5f1923fcd5f7ab
Arthur Caron <acaron@cloudbees.com>
2023-11-06 14:08:16 +0100

    [JENKINS-69917] - handle-prototype fixed.

--- plugin/src/main/resources/org/jenkinsci/plugins/workflow/cps/Snippetizer/handle-prototype.js
+++ plugin/src/main/resources/org/jenkinsci/plugins/workflow/cps/Snippetizer/handle-prototype.js
@@ -1,26 +1,36 @@
-function handlePrototype(url) {
+function handlePrototype(url, crumb) {
     buildFormTree(document.forms.config);
     // TODO JSON.stringify fails in some circumstances: https://gist.github.com/jglick/70ec4b15c1f628fdf2e9 due to Array.prototype.toJSON
-    var json = Object.toJSON(JSON.parse(document.forms.config.elements.json.value).prototype);
+    // TODO simplify when Prototype.js is removed
+    const json = Object.toJSON ? Object.toJSON(JSON.parse(document.forms.config.elements.json.value).prototype) : JSON.stringify(JSON.parse(document.forms.config.elements.json.value).prototype);
     if (!json) {
         return; // just a separator
     }
-    new Ajax.Request(url, {
-        method: 'POST',
-        parameters: {json: json},
-        onSuccess: function(r) {
-            document.getElementById('prototypeText').value = r.responseText;
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("Jenkins-Crumb", crumb);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                document.getElementById('prototypeText').value = xhr.responseText;
+            }
         }
-    });
+    };
+
+    xhr.send("json=" + encodeURIComponent(json));
 }
 
 
-document.addEventListener('DOMContentLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
 
     const generatePipelineScript = document.getElementById("generatePipelineScript");
     const url = generatePipelineScript.getAttribute("data-url");
+    const crumb = generatePipelineScript.getAttribute("data-crumb");
     generatePipelineScript.onclick = (_) => {
-        handlePrototype(url);
+        handlePrototype(url, crumb);
         return false;
     };
 
