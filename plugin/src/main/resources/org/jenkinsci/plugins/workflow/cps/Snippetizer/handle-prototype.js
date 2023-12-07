9d3888c583b10fcbad8b006d4697be25af45ddd8
Arthur Caron <acaron@cloudbees.com>
2023-12-07 14:35:41 +0100

    [JENKINS-69917] - Snippetizer/index.jelly javascript un-inlined. (#599)
    * [JENKINS-69917] - Snippetizer/index.jelly javascript un-inlined.
    
    * [JENKINS-69917] - Attribute used to pass url from jelly to JS.
    
    * [JENKINS-69917] - Apply suggestions from code review
    
    Co-authored-by: Jesse Glick <jglick@cloudbees.com>
    
    * [JENKINS-69917] - handle-prototype fixed.
    
    * [JENKINS-69917] - handle-prototype use fetch.
    
    ---------
    
    Co-authored-by: Jesse Glick <jglick@cloudbees.com>

---
+++ plugin/src/main/resources/org/jenkinsci/plugins/workflow/cps/Snippetizer/handle-prototype.js
@@ -0,0 +1,43 @@
+function handlePrototype(url, crumb) {
+    buildFormTree(document.forms.config);
+    // TODO JSON.stringify fails in some circumstances: https://gist.github.com/jglick/70ec4b15c1f628fdf2e9 due to Array.prototype.toJSON
+    // TODO simplify when Prototype.js is removed
+    const json = Object.toJSON ? Object.toJSON(JSON.parse(document.forms.config.elements.json.value).prototype) : JSON.stringify(JSON.parse(document.forms.config.elements.json.value).prototype);
+    if (!json) {
+        return; // just a separator
+    }
+
+    const headers = new Headers();
+    headers.append("Content-Type", "application/x-www-form-urlencoded");
+    headers.append("Jenkins-Crumb", crumb);
+
+    fetch(url, {
+        method: "POST",
+        headers: headers,
+        body: "json=" + encodeURIComponent(json),
+
+    })
+        .then(response => {
+            if (response.ok) {
+                response.text().then((responseText) => {
+                    document.getElementById('prototypeText').value = responseText;
+                });
+            }
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+        });
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const generatePipelineScript = document.getElementById("generatePipelineScript");
+    const url = generatePipelineScript.getAttribute("data-url");
+    const crumb = generatePipelineScript.getAttribute("data-crumb");
+    generatePipelineScript.onclick = (_) => {
+        handlePrototype(url, crumb);
+        return false;
+    };
+
+});
