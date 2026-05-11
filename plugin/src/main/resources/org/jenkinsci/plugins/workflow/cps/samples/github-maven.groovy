266ad2c4a4408c59faa2dcd37583511fbb39018a
Jesse Glick <jglick@cloudbees.com>
2026-05-11 11:22:37 -0400

    Stop proposing to use Maven tool installer in samples

--- plugin/src/main/resources/org/jenkinsci/plugins/workflow/cps/samples/github-maven.groovy
+++ plugin/src/main/resources/org/jenkinsci/plugins/workflow/cps/samples/github-maven.groovy
@@ -1,11 +1,6 @@
 pipeline {
     agent any
 
-    tools {
-        // Install the Maven version configured as "M3" and add it to the path.
-        maven "M3"
-    }
-
     stages {
         stage('Build') {
             steps {
@@ -13,10 +8,10 @@ pipeline {
                 git 'https://github.com/jglick/simple-maven-project-with-tests.git'
 
                 // Run Maven on a Unix agent.
-                sh "mvn -Dmaven.test.failure.ignore=true clean package"
+                sh 'mvn -Dmaven.test.failure.ignore=true clean package'
 
                 // To run Maven on a Windows agent, use
-                // bat "mvn -Dmaven.test.failure.ignore=true clean package"
+                // bat 'mvn -Dmaven.test.failure.ignore=true clean package'
             }
 
             post {
