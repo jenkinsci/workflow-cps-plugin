63f2f678d60b6fd3a4130712ae00850466403a3c
Jesse Glick <jglick@cloudbees.com>
2026-05-11 11:30:24 -0400

    Unused var https://github.com/jenkinsci/workflow-cps-plugin/pull/1776#discussion_r3220098922

--- plugin/src/main/resources/org/jenkinsci/plugins/workflow/cps/samples/scripted.groovy
+++ plugin/src/main/resources/org/jenkinsci/plugins/workflow/cps/samples/scripted.groovy
@@ -1,5 +1,4 @@
 node {
-    def mvnHome
     stage('Preparation') { // for display purposes
         // Get some code from a GitHub repository
         git 'https://github.com/jglick/simple-maven-project-with-tests.git'
